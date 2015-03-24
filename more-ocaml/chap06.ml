d1cea626f56abead16e56b292ed1df225b87141b
Geoff Hulette <ghulette@gmail.com>
2015-03-23 19:08:23 -0700

    Update

--- more-ocaml/chap06.ml
+++ more-ocaml/chap06.ml
@@ -27,51 +27,54 @@ let string_of_int_list l =
 let int_list_of_string s =
   explode s |> List.map int_of_char
 
+let rec rle max_len l =
+  let rec rle_aux n a = function
+    | [] -> []
+    | [x] -> (n,x)::a
+    | x::(y::_ as t) -> 
+       if x = y && n < max_len
+       then rle_aux (n+1) a t 
+       else rle_aux 1 ((n,x)::a) t
+  in
+  List.rev (rle_aux 1 [] l)
+
 (* Each run shall consist of a length byte followed by 1 to 128 bytes
 of data. If the length byte is 0-127, the following length+1 bytes are
 literal. If the length is 129-255, the following single byte shall be
 copied 257-length (2-128) times. Length of 128 denotes EOD. *)
 
+let encode l =
+  let rec encode_lits buf count acc = function
+    | (n,x)::xs when n = 1 -> encode_lits buf (count+1) (x::acc) xs
+    | l when count = 0 -> encode_run buf l
+    | l -> let len = char_of_int (count - 1) in
+           let buf' = buf @ (len::(List.rev acc)) in
+           encode_run buf' l
+  and encode_run buf = function
+    | (n,x)::xs when n > 1 ->
+       let len = char_of_int (257-n) in
+       let buf' = buf @ (len::[x]) in
+       encode_lits buf' 0 [] xs
+    | _::_ as l -> encode_lits buf 0 [] l 
+    | [] -> buf
+  in
+  (encode_run [] (rle 127 l)) @ [char_of_int 128]
+
 let rec decode = function
-  | [] -> failwith "expected EOD"
-  | [128] -> []
-  | x::xs when 0 <= x && x <= 127 ->
-     let len = x + 1 in
+  | ['\128'] -> []
+  | x::xs when '\000' <= x && x <= '\127' ->
+     let len = (int_of_char x) + 1 in
      let lits = take len xs in
      lits @ decode (drop len xs)
-  | x::d::xs when 129 <= x && x <= 255 ->
-     let n = 257 - x in
+  | x::d::xs when '\129' <= x && x <= '\255' ->
+     let n = 257 - (int_of_char x) in
      let reps = replicate n d in
      reps @ decode xs
+  | [] -> failwith "expected EOD"
   | _ -> failwith "mangled data"
 
-let rle max_run = function 
-  | [] -> []
-  | c::cs -> 
-     let f = fun a x' -> 
-       match a with 
-       | (x,n)::l -> if x = x' && n <= max_run
-                     then (x,n+1)::l 
-                     else (x',1)::(x,n)::l
-       | _ -> assert false
-     in
-     List.rev (List.fold_left f [(c,1)] cs)
-
-let encode l = 
-  let rec encode_aux a = function
-    | [] -> 
-       let len = List.length a in
-       if len > 0 then (len-1) :: (List.rev a) else []
-    | (x,n)::rs when n = 1 -> encode_aux (x::a) rs
-    | (x,n)::rs when n > 1 -> 
-       let e = encode_aux a [] in
-       e @ [257-n;x] @ (encode_aux [] rs)
-    | _ -> assert false
-  in
-  encode_aux [] (rle 127 l) @ [128]
-   
 let compress s =
-  int_list_of_string s |> encode |> string_of_int_list
+  explode s |> encode |> implode
 
 let expand s =
-  int_list_of_string s |> decode |> string_of_int_list
+  explode s |> decode |> implode
